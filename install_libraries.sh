0e8a2780013bb69b432137750e93ce86b1f0ebc6
le-codeur-rapide <paul.vezia@gmail.com>
2026-04-10 19:06:37 +0200

    working claude vllm

--- install_libraries.sh
+++ install_libraries.sh
@@ -9,3 +9,15 @@ if [ ! -d .venv ]; then
 	uv venv
 fi
 uv pip install vllm
+
+
+# claude code one liner
+curl -fsSL https://claude.ai/install.sh | bash
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc && source ~/.bashrc
+
+# setup env for claude code
+export ANTHROPIC_BASE_URL="http://localhost:8000"
+export ANTHROPIC_API_KEY="dummy"
+export ANTHROPIC_DEFAULT_OPUS_MODEL="my-model"
+export ANTHROPIC_DEFAULT_SONNET_MODEL="my-model"
+export ANTHROPIC_DEFAULT_HAIKU_MODEL="my-model"
---
+++ launch_vllm.sh
@@ -0,0 +1,4 @@
+
+
+
+uv run vllm serve openai/gpt-oss-20b --served-model-name my-model --enable-auto-tool-choice --tool-call-parser openai

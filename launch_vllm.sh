d804fdc1cd9f50e66770a41298a0c4820ee34f3c
le-codeur-rapide <paul.vezia@gmail.com>
2026-04-11 12:33:01 +0200

    opencode config

--- launch_vllm.sh
+++ launch_vllm.sh
@@ -7,7 +7,7 @@ mkdir -p /dev/shm/vllm-models
 uv run vllm serve --model Jackrong/Qwen3.5-27B-Claude-4.6-Opus-Reasoning-Distilled --tokenizer Qwen/Qwen3.5-27B \
     --host 0.0.0.0 --port 8000 --dtype bfloat16 --enable-prefix-caching \
     --kv-cache-dtype fp8 --max-model-len 210000 --chat-template-content-format string \
-    --enable-auto-tool-choice --tool-call-parser qwen3_coder --reasoning-parser qwen3 --download-dir /dev/shm/vllm-models
+    --enable-auto-tool-choice --tool-call-parser qwen3_coder --reasoning-parser qwen3 --download-dir /dev/shm/vllm-models --quantization fp8 
 
 uv run vllm serve Jackrong/Qwen3.5-27B-Claude-4.6-Opus-Reasoning-Distilled   \
     --tokenizer Qwen/Qwen3.5-27B   \
@@ -17,4 +17,5 @@ uv run vllm serve Jackrong/Qwen3.5-27B-Claude-4.6-Opus-Reasoning-Distilled   \
     --gpu-memory-utilization 0.90 \
     --served-model-name my-model   \
     --enable-auto-tool-choice   \
-    --tool-call-parser openai
+    --tool-call-parser openai   \
+    --download-dir /dev/shm/vllm-models

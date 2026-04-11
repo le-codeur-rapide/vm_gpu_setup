2fada48e5f1873e25c55453077e396556568d9af
le-codeur-rapide <paul.vezia@gmail.com>
2026-04-11 11:42:50 +0200

    update

--- install_libraries.sh
+++ install_libraries.sh
@@ -14,3 +14,4 @@ uv pip install git+https://github.com/huggingface/transformers.git --upgrade
 
 # install opencode
 curl -fsSL https://opencode.ai/install | bash
+source ~/.bashrc
--- launch_vllm.sh
+++ launch_vllm.sh
@@ -1,4 +1,20 @@
 
+# to download models in ram
+mkdir -p /dev/shm/vllm-models
 
+# uv run vllm serve openai/gpt-oss-20b --served-model-name my-model --enable-auto-tool-choice --tool-call-parser openai
 
-uv run vllm serve openai/gpt-oss-20b --served-model-name my-model --enable-auto-tool-choice --tool-call-parser openai
+uv run vllm serve --model Jackrong/Qwen3.5-27B-Claude-4.6-Opus-Reasoning-Distilled --tokenizer Qwen/Qwen3.5-27B \
+    --host 0.0.0.0 --port 8000 --dtype bfloat16 --enable-prefix-caching \
+    --kv-cache-dtype fp8 --max-model-len 210000 --chat-template-content-format string \
+    --enable-auto-tool-choice --tool-call-parser qwen3_coder --reasoning-parser qwen3 --download-dir /dev/shm/vllm-models
+
+uv run vllm serve Jackrong/Qwen3.5-27B-Claude-4.6-Opus-Reasoning-Distilled   \
+    --tokenizer Qwen/Qwen3.5-27B   \
+    --quantization fp8   \
+    --dtype float16   \
+    --max-model-len 8192   \
+    --gpu-memory-utilization 0.90 \
+    --served-model-name my-model   \
+    --enable-auto-tool-choice   \
+    --tool-call-parser openai
